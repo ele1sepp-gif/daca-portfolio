b88ae795e169b6ffd6e20d2b703ba0d888fc94ac
ele1sepp-gif <ele1sepp@gmail.com>
2026-05-21 14:32:58 +0300

    Update week3[domeen]joins.sql

--- week-3/individual/week3[domeen]joins.sql
+++ week-3/individual/week3[domeen]joins.sql
@@ -1,1 +1,16 @@
+ROLL A Müük+Kliendid:
+  
+Analüüs näitab, et ainult 29,91% klientidest kulutab üle keskmise, mis viitab sellele, et UrbanStyle’i käive sõltub suuresti väiksemast grupist väärtuslikest klientidest.
+Seetõttu peaks ettevõte keskenduma lojaalsete ja suure kulutusega klientide hoidmisele ning pakkuma neile personaalseid kampaaniaid ja lojaalsusprogramme. 
+Samal ajal võiks ettevõte proovida motiveerida väiksema kulutusega kliente rohkem ostma, et kasvatada üldist müüki.
+Analüüsi põhjal on UrbanStylei tugevaim müügipiirkond Tallinn, mis toob ettevõttele kõige suurema käibe ja kliendiarvu.
+Tartu on samuti oluline turg ning seal võiks kaaluda täiendavaid turunduskampaaniaid, et kasvatada müüki veelgi.
+TOP-klientide analüüs näitab, et osa kliente teeb väga sagedasi oste või kulutab märkimisväärselt palju raha, mis viitab lojaalsete püsiklientide suurele väärtusele ettevõtte jaoks.
+Samuti loyalty tier analüüs, et kõrgema taseme kliendid kulutavad keskmiselt rohkem, seega tasub lojaalsusprogrammidesse edasi investeerida.
 
+ROLL D Müügikanalid+ Kliendid
+
+Analüüsi põhjal teenivad UrbanStylei füüsilised poed rohkem tulu kui online kanal, mis näitab, et kliendid eelistavad endiselt kohapealset ostlemist.
+Kõige tugevam müügipiirkond on Tallinn, mistõttu võiks sinna suunata suurema osa turunduseelarvest.
+Samal ajal võiks väiksemates linnades proovida täiendavaid kampaaniaid või tugevdada online turundust, et kasvatada müüki.
+Meeste riided on kõige populaarsem kategooria, kuid kuna erinevused teiste kategooriatega ei ole väga suured, peaks ettevõte jätkama mitmekesist turundust kõikide toodete jaoks.
